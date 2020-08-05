2331ab3fbc949a0a71150868ede615567bf5976f
Daniel Wheeler <danelhombre@gmail.com>
2020-08-04 17:09:32 -0700

    Ho rifatto il controller del player. Ora usa la fisica integrata

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,51 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    public bool isMoving = true;
-    public bool isGrounded = false;
     public float playerSpeed = 5f;
-    public float jumpHeight = 1f;
+    public float jumpForce = 5f;
 
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private float gravityValue = -9.81f;
+    [SerializeField]
+    private bool isMoving = true;
+    [SerializeField]
+    private bool isGrounded = false;
+
+    private Rigidbody rigidBody;
+    private Vector3 direction;
+    private Vector3 movementForce;
+    private Vector3 movementVelocity;
+
 
     void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        rigidBody        = GetComponent<Rigidbody>();
+        direction        = Vector3.right;
+        movementVelocity = direction * playerSpeed;
     }
 
-    void Update()
+    private void Update()
     {
-        // Direction
-        Vector3 direction = Vector3.forward;
-        Vector3 velocity = direction * playerSpeed;
-
-        if (controller.isGrounded && !isGrounded)
-            isGrounded = controller.isGrounded;
-
-        if (isGrounded && playerVelocity.y < 0)
-            playerVelocity.y = 0f;
-
+        // Run forward.
         if (isMoving)
         {
-            Vector3 move = new Vector3(playerSpeed, 0, 0);
-            controller.Move(move * Time.deltaTime);
-            gameObject.transform.forward =  move;
+            rigidBody.velocity = new Vector3(playerSpeed, rigidBody.velocity.y, 0);
         }
 
-        // Changes the height position of the player..
+        // Changes the height position of the player.
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+            movementForce = Vector3.up * jumpForce;
+            rigidBody.AddForce(movementForce, ForceMode.Impulse);
             isGrounded = false;
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
+    }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit()
+    {
+        isGrounded = false;
     }
 }
